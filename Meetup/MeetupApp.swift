a762743e13e23f9114908f84008f792cb8d74f57
Razan <razanalzannan@gmail.com>
2023-05-25 22:44:40 +0300

    Initial Commit

---
+++ Meetup/MeetupApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeetupApp.swift
+//  Meetup
+//
+//  Created by Razan Mohammed Alzannan on 05/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct MeetupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
